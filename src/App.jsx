9494a6f77346315af0b36f12c39d20bf5f474099
chooing <chumil7432@gmail.com>
2022-07-18 13:53:11 +0900

    #42 - Fix : 네비게이션 바 조건 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SearchPage from './pages/SearchPage';
 import MyProfile from './pages/MyProfile';
-import NavigatorMenu from './components/navigator/NavigatorMenu';
 import ErrorPage from './pages/404Page';
 import Splash from './pages/Splash';
 import SplashLogin from './pages/SplashLogin';
@@ -12,7 +10,6 @@ import SplashLogin from './pages/SplashLogin';
 function App() {
 	return (
 		<>
-			<NavigatorMenu />
 			<Routes>
 				<Route path="/login" exact element={<LoginPage />} />
 				<Route path="/search" exact element={<SearchPage />} />
