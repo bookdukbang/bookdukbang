d12f98c248bf4d747c428925f5b4037e8c6adcb3
minyoung22222 <alsdudr97@naver.com>
2022-07-29 00:35:11 +0900

    #113 : Fix: 내 프로필 상품 css 수정 & 유저프로필 사진 수정

--- src/components/feed/UserFollowing.jsx
+++ src/components/feed/UserFollowing.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { SERVER_URL } from '../../constants';
 import { NoneProfileSmall } from '../common/user/UserUpload';
@@ -53,11 +54,13 @@ function UserFollowing() {
 			{Following !== null &&
 				Following?.map((item, i) => (
 					<UserLi key={i}>
-						<NoneProfileSmall
-							style={{
-								backgroundImage: `url(${item.image})`,
-							}}
-						/>
+						<Link to={`/user/${item.accountname}`}>
+							<NoneProfileSmall
+								style={{
+									backgroundImage: `url(${item.image})`,
+								}}
+							/>
+						</Link>
 						<UserInfo>
 							{item.username}
 							<p>@ {item.accountname}</p>
